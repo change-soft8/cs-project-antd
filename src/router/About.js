accfc72b59f79bd208886176d9cc9c0b7077f9a1
unknown <raoenhui>
2016-10-14 17:15:37 +0800

    add antd

--- src/router/About.js
+++ src/router/About.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { DatePicker, message } from 'antd';
+import 'antd/dist/antd.css';
 
-const About = React.createClass({
-  render() {
-    return <h1>About111</h1>
-  }
-})
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: '',
+        };
+    }
+    handleChange(date) {
+        message.info('您选择的日期是: ' + date.toString());
+        this.setState({ date });
+    }
+    render() {
+        return (
+            <div style={{ width: 400, margin: '100px auto' }}>
+		        <DatePicker onChange={value => this.handleChange(value)} />
+		        <div style={{ marginTop: 20 }}>当前日期：{this.state.date.toString()}</div>
+		    </div>
+        );
+    }
+}
+
 
 module.exports = About
